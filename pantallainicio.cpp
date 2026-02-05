138c8eb852e527592aecb055f54324a538274f4c
Pablo_Maldonado <pablomaldonadodev@gmail.com>
2026-02-05 15:33:03 -0600

    Se han definido los botones de la interfaz principal

--- pantallainicio.cpp
+++ pantallainicio.cpp
@@ -6,6 +6,14 @@ PantallaInicio::PantallaInicio(QWidget *parent)
     , ui(new Ui::PantallaInicio)
 {
     ui->setupUi(this);
+
+    //Se activa el estilo
+    this->setAttribute(Qt::WA_StyledBackground, true);
+
+    //Aplicamos el estilo solo al padre (#) y usamos border-image para redimensionar
+    this->setStyleSheet("QWidget#PantallaInicio { "
+                        "border-image: url(:/assets/layoutsGame/LayoutInicial.png) 0 0 0 0 stretch stretch; "
+                        "} ");
 }
 
 PantallaInicio::~PantallaInicio()
